644c5147b56ab41b53b380c093592af6cfe961d7
Collinsac <agucollins832@gmail.com>
2025-01-11 05:33:55 +0100

    chap 8

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-require('dotenv').config();
-require('./libs/dbConnect');
+require("dotenv").config();
+require("./libs/dbConnect");
+
+const userRouter = require("./routes/user.route");
 
 const app = express();
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.use(morgan('dev'));
-app.use(express.static('./public'));
+app.use(morgan("dev"));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.render('index', { message: 'Hello From Node.js' });
+app.get("/", (req, res) => {
+  res.render("index", { message: "Hello From Node.js" });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('index', { message: 'The Contact Page' });
+app.use("/users", userRouter);
+
+app.get("/contact", (req, res) => {
+  res.render("index", { message: "The Contact Page" });
 });
 
-app.get('/about', (req, res) => {
-  res.render('index', { message: 'The About Page' });
+app.get("/about", (req, res) => {
+  res.render("index", { message: "The About Page" });
 });
 
-app.get('*', (req, res) => {
-  res.status(404).render('index', { message: 'Not Found' });
+app.get("*", (req, res) => {
+  res.status(404).render("index", { message: "Not Found" });
 });
 
 const PORT = 3000;
